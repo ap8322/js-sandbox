6b45505bae89c4a4986902c444ee6ffcb727b404
haneda <yuki.haneda@bizreach.co.jp>
2017-09-23 20:14:51 +0900

    simple karma test

--- spec/src/calc-spec.js
+++ spec/src/calc-spec.js
@@ -1,11 +1,13 @@
 describe('関数のテスト', function() {
-  // 成功するテスト
   it('1 + 1 = 2である', function(){
     expect(add(1, 1)).toBe(2);
   });
 
-  // 失敗するテスト
   it('1 + 2 = 3である', function(){
     expect(add(1, 2)).toBe(3);
+  });
+
+  it('1 + 3 = 4である', function(){
+    expect(add(1, 3)).toBe(4);
   });
 });
