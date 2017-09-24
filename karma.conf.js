988e3d5336ac4ecf1b29d600fb9aba1c32d89a25
haneda <yuki.haneda@bizreach.co.jp>
2017-09-24 13:53:20 +0900

    add coverage,junit,webpack

--- karma.conf.js
+++ karma.conf.js
@@ -28,14 +28,26 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'src/*.js': ['coverage']
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['mocha'],
+    reporters: ['mocha','coverage','junit'],
 
+    junitReporter: {
+        outputDir: 'report'
+    },
+
+    coverageReporter: {
+        dir: 'report',
+        reporters: [
+            { type: 'html' },
+            { type: 'cobertura' }
+        ]
+    },
 
     // web server port
     port: 9876,
